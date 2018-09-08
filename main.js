35cc896847b576998720611f2a388e46fea982b9
Jongwoo Kim <kjwoo810@gmail.com>
2018-09-08 21:14:40 +0900

    retrieve location of store

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ const read_html = request(url, (err,res,data) => {
   console.log('okay cheerio!');
 
   let $ = cheerio.load(data);
-  let json = [], label, pubName
-  $('body > div.container-fluid > div > table > tbody > tr').each(function(index, ele){
+  let json = [], label, pubName, location
+  $('body > div.container-fluid > div > table > tbody > tr').each(function(index, ele)
+  {
     label = $(this).find('tr > th:nth-child(1) > span').text()
     pubName = $(this).find('tr > th:nth-child(2) > a').text()
-    json.push({lable: label, pubName: pubName})
+    location = $(this).find('tr > th:nth-child(3) > a:nth-child(2)').attr('href');
+    json.push({ "lable" : label, "pubName" : pubName, "location" : location })
   });
 
   console.log('json: ', json);
