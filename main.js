580a832f3889088ec7a718dcd14e2293c5495a9b
Jongwoo Kim <kjwoo810@gmail.com>
2018-09-24 22:24:16 +0900

    extract beerPub-address

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var url = "https://alcohol-map.herokuapp.com/request";
-let resultArr = [];
+let locationArr = [];
+
 
 const read_html = request(url, (err,res,data) => {
   err? console.log(err):
@@ -18,6 +19,11 @@ const read_html = request(url, (err,res,data) => {
     json.push({ "lable" : label, "pubName" : pubName, "location" : location })
   });
 
-  console.log('json: ', json);
+  //console.log('json: ', json);
+  for(let i = 0; i < json.length; i++)
+  {
+    locationArr[i] = json[i].location.replace("https://www.google.com/maps/dir//", "");
+  }
+  console.log(locationArr);
 
 });
